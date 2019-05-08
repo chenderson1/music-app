1836b6231d99ca95ad3e5f1419e42bda7970afcb
Cory Henderson <cory.henderson07@gmail.com>
2019-05-08 15:56:46 -0600

    before merge

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 config();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import expressJwt from "express-jwt";
 
 const port = process.env.PORT;
 
@@ -27,9 +28,19 @@ mongoose.connect(
   }
 );
 
+//user login/signup routes
+app.use("/auth", require("./routes/authRoutes"));
+//every /api route request jwt will verify token
+app.use("/api", expressJwt({ secret: process.env.SECRET }));
+//main resourse routes
+
 //global error handler
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.log(err);
+  if (err.name === "UnauthorizedError") {
+    // express-jwt gives the 401 status to the err object for us
+    res.status(err.status);
+  }
   return res.send({ errMsg: err.message });
 });
 
