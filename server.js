7e08fa8ecbb2588e9b82a694bc25bbfe8d325c46
Cory Henderson <cory.henderson07@gmail.com>
2019-05-08 15:13:31 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import "colors";
+import { config } from "dotenv";
+config();
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+const port = process.env.PORT;
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+//connect to DB
+mongoose.connect(
+  "mongodb://dt031512:dt031512@ds153566.mlab.com:53566/music-app",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  },
+  () => {
+    console.log("Connected to the DB".rainbow);
+  }
+);
+
+//global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  return res.send({ errMsg: err.message });
+});
+
+//server listen
+app.listen(port, () =>
+  console.log(`The Magic Happened on Port:${port}`.random)
+);
