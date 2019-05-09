ec34d862a68526e6512d3b5e81f417d660132375
Cory Henderson <cory.henderson07@gmail.com>
2019-05-09 15:03:17 -0600

    before merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import UserDataProvider from "./context/userDataProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <UserDataProvider>
+    <App />
+  </UserDataProvider>,
+  document.getElementById("root")
+);
