0ec8f0557db9f12af81ff41f0364d8c65b56944b
Cory Henderson <cory.henderson07@gmail.com>
2019-07-01 08:17:38 -0700

    added express static script

--- server.js
+++ server.js
@@ -33,6 +33,17 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //user login/signup routes
 app.use("/auth", require("./routes/authRoutes"));
 //every /api route request jwt will verify token
